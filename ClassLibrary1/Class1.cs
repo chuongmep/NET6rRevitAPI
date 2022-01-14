19a5b4e0befc96fc3ce09a4754c69239f8b34aba
Chuong Ho <31106432+chuongmep@users.noreply.github.com>
2022-01-14 12:41:19 +0700

    Initial commit

---
+++ ClassLibrary1/Class1.cs
@@ -0,0 +1,17 @@
+﻿using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+
+namespace ClassLibrary1;
+
+[Transaction( TransactionMode.Manual)]
+public class Class1 : IExternalCommand
+{
+    public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+    {
+        TaskDialog.Show("Hello","This is a content");
+        var parse = (string s) => int.Parse(s);
+        TaskDialog.Show("Test", parse.ToString());
+        return Result.Succeeded;
+    }
+}
