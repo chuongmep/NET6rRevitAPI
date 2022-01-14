181159e9f7926b33b52a3d951c39a4afc8a7e247
Chuong Ho <31106432+chuongmep@users.noreply.github.com>
2022-01-14 12:57:06 +0700

    test assembly

--- ClassLibrary1/Class1.cs
+++ ClassLibrary1/Class1.cs
@@ -1,17 +1,35 @@
-﻿using Autodesk.Revit.Attributes;
+﻿using System.Reflection;
+using System.Runtime.Loader;
+using Autodesk.Revit.Attributes;
 using Autodesk.Revit.DB;
 using Autodesk.Revit.UI;
 
 namespace ClassLibrary1;
 
-[Transaction( TransactionMode.Manual)]
+[Transaction(TransactionMode.Manual)]
 public class Class1 : IExternalCommand
 {
     public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
     {
-        TaskDialog.Show("Hello","This is a content");
-        var parse = (string s) => int.Parse(s);
-        TaskDialog.Show("Test", parse.ToString());
-        return Result.Succeeded;
+        for (;;)
+        {
+            var context = new SimpleUnloadableAssemblyLoadContext();
+            Assembly assembly = context.LoadFromAssemblyPath(@"D:\API\Revit\xxx.dll");
+            int executeAssembly = ExecuteAssembly(assembly, Array.Empty<string>());
+            TaskDialog.Show("Test", executeAssembly.ToString());
+            context.Unload();
+            return Result.Succeeded;
+        }
+
+        int ExecuteAssembly(Assembly assembly, string[] args)
+        {
+            MethodInfo? entry = assembly.EntryPoint;
+
+            object? result = entry == null || entry.GetParameters().Length <= 0
+                ? entry?.Invoke(null, null)
+                : entry.Invoke(null, new object[] {args});
+
+            return (result != null) ? (int) result : 0;
+        }
     }
 }
---
+++ ClassLibrary1/SimpleUnloadableAssemblyLoadContext.cs
@@ -0,0 +1,14 @@
+﻿using System.Reflection;
+using System.Runtime.Loader;
+
+namespace ClassLibrary1;
+
+public class SimpleUnloadableAssemblyLoadContext : AssemblyLoadContext
+{
+    public SimpleUnloadableAssemblyLoadContext()
+        : base(isCollectible: true)
+    {
+    }
+
+    protected override Assembly? Load(AssemblyName assemblyName) => null;
+}
